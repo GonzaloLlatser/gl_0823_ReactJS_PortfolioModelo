a2b47fc47d5d39881a066108c701b74887000dea
GonzaloLlatser <gonllat@gmail.com>
2023-08-11 20:21:38 +0200

    style: Muestra imagenes, diseña tarjetas del potafolio, maquetacion general de los elementos.

---
+++ src/data/Trabajos.js
@@ -0,0 +1,56 @@
+export const trabajos =[
+    {
+        'id':'AppdePeliculas',
+        'nombre': 'WEB1',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    },
+    
+    {
+        'id':'prueba2',
+        'nombre': 'WEB2',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    },
+
+    {
+        'id':'prueba3',
+        'nombre': 'WEB3',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    },
+
+    {
+        'id':'prueba4',
+        'nombre': 'WEB4',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    },
+
+    {
+        'id':'prueba5',
+        'nombre': 'WEB5',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    },
+
+    {
+        'id':'prueba6',
+        'nombre': 'WEB6',
+        'url':'google.com',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo Web',
+        'descripcion':'Te cuento del proyecto'
+    }
+
+];
