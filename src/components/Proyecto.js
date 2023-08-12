6d361991064e8d7fbf7fad880c6537220d2799f5
GonzaloLlatser <gonllat@gmail.com>
2023-08-12 09:41:20 +0200

    style: Da toques finales a la pagina de detalle, se verifica url correctas.

--- src/components/Proyecto.js
+++ src/components/Proyecto.js
@@ -11,19 +11,17 @@ export const Proyecto = () => {
   useEffect(() => {
     let proyecto = trabajos.filter(trabajo => trabajo.id === params.id);
     setProyecto(proyecto[0]);
-
-    console.log(proyecto);
-
   }, [])
 
   return (
-    <div className='page'>
+    <div className='page page-work'>
       <div className='mask'>
         <img src={'/images/' + proyecto.id + '.png'} />
       </div>
       <h1 className='heading'>Proyecto {proyecto.nombre}</h1>
       <p>{proyecto.tecnologias}</p>
       <p>{proyecto.descripcion}</p>
+      <a href={'https://'+proyecto.url} target='_blank'>Ir al Proyecto</a>
     </div>
   )
 }
--- src/data/Trabajos.js
+++ src/data/Trabajos.js
@@ -1,56 +1,56 @@
-export const trabajos =[
+export const trabajos = [
     {
-        'id':'AppDePeliculas',
-        'nombre':'Aplicación de Peliculas',
-        'url':'google.com',
+        'id': 'AppDePeliculas',
+        'nombre': 'Aplicación de Peliculas',
+        'url': 'github.com/GonzaloLlatser/gl_0823_ReactJS_AplicacionDePeliculas.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea una aplicación en donde el usuario puede incluir títulos de películas junto con su descripción correspondiente. Además, se genera la funcionalidad de poder editar, borrar y buscar las películas de la lista.'
     },
-    
+
     {
-        'id':'AppCalculadora',
+        'id': 'AppCalculadora',
         'nombre': 'Calculadora Interactiva',
-        'url':'google.com',
+        'url': 'github.com/GonzaloLlatser/gl_0723_ReactJS_CalculadoraInteractiva.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea una calculadora, utilizando para ello REACT y minimizando al maximo, el uso de HTML.'
     },
 
     {
-        'id':'AppDeTareas',
+        'id': 'AppDeTareas',
         'nombre': 'Aplicación de Tareas',
-        'url':'google.com',
+        'url': 'github.com/GonzaloLlatser/gl_0723_ReactJS_AplicacionDeTareas.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea una aplicación de tareas, utilizando para ello REACT. En esta app, el usuario puede crear, dar por hecha o eliminar una tarea de su lista.'
     },
 
     {
-        'id':'AppContador',
+        'id': 'AppContador',
         'nombre': 'Contador de clicks',
-        'url':'google.com',
+        'url': 'github.com/GonzaloLlatser/gl_0723_ReactJS_ContadorClics.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea un contador de clic, utilizando para ello REACT y minimizando al maximo, el uso de HTML.'
     },
 
     {
-        'id':'ClonPaginaWeb',
+        'id': 'ClonPaginaWeb',
         'nombre': 'Clon de Página Web',
-        'url':'google.com',
+        'url': 'github.com/GonzaloLlatser/gl_0723_ReactJS_ClonWeb.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea una pantalla similar a la de una web académica, utilizando para ello REACT y minimizando al maximo, el uso de HTML.'
     },
 
     {
-        'id':'Game',
+        'id': 'Game',
         'nombre': 'Rock Paper Scissors',
-        'url':'google.com',
+        'url': 'github.com/GonzaloLlatser/gl_0723_javascript_RockPaperScissors.git',
         'tecnologias': 'HTML, CSS, JS, REACT',
         'categorias': 'Desarrollo Web',
-        'descripcion':'Te cuento del proyecto'
+        'descripcion': 'En este proyecto, a modo de práctica, se crea el jugo Piedra-Papel-Tijera.'
     }
 
 ];
