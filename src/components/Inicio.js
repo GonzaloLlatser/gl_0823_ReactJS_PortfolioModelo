2770e44ff63b4ef4e3426ac8e5460128d4bea30f
GonzaloLlatser <gonllat@gmail.com>
2023-08-12 10:11:55 +0200

    style: Aplica en la pantalla de inicio, proyectos filtrados.

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadoTrabajos'
 
 export const Inicio = () => {
   return (
@@ -8,7 +9,7 @@ export const Inicio = () => {
 
       <h2>
         Hola, soy <strong>Gonzalo Llatser</strong> y estoy en proceso de convertirme en<strong> Desarrollador Web. </strong>
-        Actualmente estoy cursando la FP de <strong>DAW</strong> en Valencia, España. 
+        Actualmente estoy cursando la FP de <strong>DAW</strong> en Valencia, España.
       </h2>
 
       <h3>Te ayudo a crear tu sitio web, tener más visibilidad y relevancia en internet. <Link to='/contacto'>Contacta conmigo</Link></h3>
@@ -16,7 +17,8 @@ export const Inicio = () => {
       <section className='last-works'>
         <h2 className='heading'>Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos de desarrollo web</p>
-        <div className='works'></div>
+
+        <ListadoTrabajos limite ='3'/>
       </section>
     </div>
   )
--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { trabajos } from '../data/Trabajos.js';
 import { Link } from 'react-router-dom';
+import{ListadoTrabajos} from './ListadoTrabajos'
 
 export const Portafolio = () => {
   return (
@@ -8,23 +8,10 @@ export const Portafolio = () => {
 
       <h1 className='heading'>Portafolio</h1>
 
-      <section className='works'>
-        {
-          trabajos.map(trabajo => {
-            return (
-              <article key={trabajo.id} className='work-item'>
-                <div className='mask'>
-                  <img src={'/images/' + trabajo.id + '.png'} />
-                </div>
-                <span>{trabajo.categorias}</span>
-                <h2>
-                  <Link to={'/proyecto/' + trabajo.id}>{trabajo.nombre}</Link></h2>
-                <h3>{trabajo.tecnologias}</h3>
-              </article>
-            );
-          })
-        }
-      </section>
+
+      <ListadoTrabajos />
+
+
 
 
     </div>
